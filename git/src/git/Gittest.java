f28f52a08e279184e3e57d053dc994f6e2dc4999
smart <smart@DESKTOP-FOPBHJ0>
2023-06-26 19:47:18 +0530

    initial commit

---
+++ git/src/git/Gittest.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hi welcome to git hub push testing");
+	}
+
+}
